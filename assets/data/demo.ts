8bb948eb319bf6cc7a4bd35aad6a257dfa0b0f4c
andre-developer12 <andre_alves12@outlook.com>
2021-10-12 23:01:17 -0300

    inicio app

---
+++ assets/data/demo.ts
@@ -0,0 +1,129 @@
+import { DataT } from "../../types";
+import IMAGE_01 from "../images/01.jpg";
+import IMAGE_02 from "../images/02.jpg";
+import IMAGE_03 from "../images/03.jpg";
+import IMAGE_04 from "../images/04.jpg";
+import IMAGE_05 from "../images/05.jpg";
+import IMAGE_06 from "../images/06.jpg";
+import IMAGE_07 from "../images/07.jpg";
+import IMAGE_08 from "../images/08.jpg";
+import IMAGE_09 from "../images/09.jpg";
+import IMAGE_10 from "../images/10.jpg";
+
+const data: DataT[] = [
+  {
+    id: 1,
+    name: "Pépe",
+    isOnline: true,
+    match: "78",
+    description:
+      "Sou divertido e brincalhão, gosto de correr.",
+    message:
+      "Estou a procura de um AMOR.",
+    image: IMAGE_01,
+  },
+  {
+    id: 2,
+    name: "Fusquinha",
+    match: "93",
+    description:
+      "Sou divertido e brincalhão, gosto de correr.",
+    isOnline: false,
+    message: "Estou a procura de um AMOR.",
+    image: IMAGE_02,
+  },
+  {
+    id: 3,
+    name: "Bely",
+    match: "45",
+    description:
+      "Sou divertido e brincalhão, gosto de correr.",
+    isOnline: false,
+    message:
+      "Estou a procura de um AMOR.",
+    image: IMAGE_03,
+  },
+  {
+    id: 4,
+    name: "Bethoven",
+    match: "88",
+    description:
+      "Sou divertido e brincalhão, gosto de correr.",
+    isOnline: true,
+    message: "Estou a procura de um AMOR.",
+    image: IMAGE_04,
+  },
+  {
+    id: 5,
+    name: "Bob",
+    match: "76",
+    description:
+      "Sou divertido e brincalhão, gosto de correr",
+    isOnline: false,
+    message: "Estou a procura de um AMOR",
+    image: IMAGE_05,
+  },
+  {
+    id: 6,
+    name: "Luna",
+    match: "95",
+    description:
+      "Sou divertido e brincalhão, gosto de correr.",
+    isOnline: true,
+    message:
+      "Estou a procura de um AMOR.",
+    image: IMAGE_06,
+  },
+  {
+    id: 7,
+    name: "Spayk",
+    match: "67",
+    description:
+      "Sou divertido e brincalhão, gosto de correr.",
+    isOnline: true,
+    message:
+      "Estou a procura de um AMOR.",
+    image: IMAGE_07,
+  },
+  {
+    id: 8,
+    name: "Nicky",
+    match: "85",
+    description:
+      "Sou divertido e brincalhão, gosto de correr.",
+    age: "27",
+    location: "Irvine, CA",
+    info1: 'Straight, Single, 5"10',
+    info2: "Tea Totaller, Loves Photography & Travel",
+    info3: "Beaches, Mountain, Cafe, Movies",
+    info4: "Last seen: 23h ago",
+    isOnline: true,
+    message:
+      "Estou a procura de um AMOR.",
+    image: IMAGE_08,
+  },
+  {
+    id: 9,
+    name: "Milla",
+    match: "74",
+    description:
+      "Sou divertido e brincalhão, gosto de corre.r",
+    isOnline: true,
+    message:
+      "Estou a procura de um AMOR.",
+    image: IMAGE_09,
+  },
+  {
+    id: 10,
+    name: "Thor",
+    match: "98",
+    description:
+      "Sou divertido e brincalhão, gosto de correr.",
+    isOnline: false,
+    message:
+      "Estou a procura de um AMOR.",
+    image: IMAGE_10,
+  },
+];
+
+export default data;
